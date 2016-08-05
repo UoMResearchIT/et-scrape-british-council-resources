4742bfdcdc8008a2e0f05c607bcfdf31e8981f68
Rob Dunne <rob.dunne@manchester.ac.uk>
2016-08-05 13:48:10 +0100

    Video and style updates

--- bcfetch.py
+++ bcfetch.py
@@ -20,6 +20,7 @@ from bs4 import BeautifulSoup
 from bcprocess import ProcessContent
 import requests
 import os
+import html5lib
 
 class BCFetch():
 	'Class for wrapping resource content in the eTekkatho HTML header and footer'
@@ -40,7 +41,7 @@ class BCFetch():
 				homepage = ''
 				page = requests.get(self.startURL+str(i)).text
 				
-				html = BeautifulSoup(page, 'html.parser')
+				html = BeautifulSoup(page, 'html5lib')
 				html.prettify()
 				for link in html.findAll('a', href=True):
 					# Filter the list of links for the relevant sub-pages
@@ -52,7 +53,7 @@ class BCFetch():
 						self.contentURLs.append(ahref)
 			
 				# Build the home page
-				homepage += str(html.find_all('div', class_="content-inner"))
+				homepage += str(html.find('div', class_="content-inner"))
 			
 			# Makes the links relative
 			homepage = BeautifulSoup(homepage, 'html.parser')
@@ -93,8 +94,9 @@ class BCFetch():
 			print('Working on: ', url)
 			# Get the page content
 			content = ''
-			page = requests.get('http://learnenglishteens.britishcouncil.org'+url).text
-			html = BeautifulSoup(page, 'html.parser')
+			videoFound = False
+			page = requests.get('http://learnenglishteens.britishcouncil.org'+url).text	
+			html = BeautifulSoup(page, 'html5lib')
 			html.prettify()
 			content = html.findAll("div", {"id": "node-article-full-group-content"})
 			
@@ -111,6 +113,7 @@ class BCFetch():
 			# Save media
 			for video in content[0].findAll('video', src=True):
 				print('>>>> Video found')
+				videoFound = True
 				# Makes the links relative
 				videoURL = video['src']
 				videoURL = videoURL.split('?', 1)[0]
@@ -121,10 +124,50 @@ class BCFetch():
 				
 				# Update the path
 				video['src'] = video['src'].replace(video['src'], videofilename)
+  			
+			if not videoFound:
+				dataVid = content[0].find("div", {"class": "viddler-auto-embed"})
+				print('>>>> Viddler found')
+				vidID = dataVid['data-video-id']
+				
+				print('http://www.viddler.com/file/p/'+vidID+'.mp4?vfid=7782055d4424d3d4aadf6dbd12ffa429&ec_rate=63.695603550295864&ec_prebuf=60')
+				
+				# Save the video
+				try:
+					urllib.request.urlretrieve('http://www.viddler.com/file/p/'+vidID+'.mp4?vfid=7782055d4424d3d4aadf6dbd12ffa429&ec_rate=63.695603550295864&ec_prebuf=60', 'resources/pages/'+subdir+'/'+vidID+'.mp4') 
+								
+					videoHTML = '<video width="100%" x-webkit-airplay="allow" type="video/mp4" src="'+vidID+'.mp4" controls="controls"></video>'
+				
+					mediaContainer = content[0].find("div", {"class": "field-name-field-media"})
+					mediaContainer.insert(1, videoHTML)
+				except urllib.error.HTTPError:
+					print('Error fetching the video file.')
+  					
+  			# Save files
+			for file in content[0].findAll("span", {"class": "file"}):
+				print('>>>> File found')
+				
+				a = file.find('a')
+				filename = a['href'].rsplit('/', 1)[-1]
+				print(filename)
+				
+				# Save the file
+				urllib.request.urlretrieve(a['href'], 'resources/pages/'+subdir+'/'+filename) 
+				
+				# Update the path
+				a['href'] = a['href'].replace(a['href'], filename)
+  			
+  			# Tidy up and remove excess content
+			[s.extract() for s in content[0].find_all('div',  {"id": "footer-nav"})]
+			[s.extract() for s in content[0].find_all('div',  {"class": "field-name-comment-count"})]
+			[s.extract() for s in content[0].find_all('div',  {"class": "field-collection-container"})]
+			[s.extract() for s in content[0].find_all('div',  {"class": "field-name-rate"})]
+			[s.extract() for s in content[0].find_all('div',  {"id": "node-article-full-group-taxonomy"})]
+			[s.extract() for s in content[0].find_all('div',  {"class": "sharethis-buttons"})]
   				
 			# Save the page
 			pc = ProcessContent()
-			pc.wrapAndWrite(str(content), subdir)
+			pc.wrapAndWrite(str(content[0]), subdir)
 
 BCFetch()    
     
